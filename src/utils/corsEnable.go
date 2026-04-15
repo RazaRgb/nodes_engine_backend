1d17cfe1b5031987c74d8ad3ace671c99cd05525
RazaRgb <razaraza8143@gmail.com>
2026-04-15 14:12:44 +0530

    handling cors for deployment

--- src/utils/corsEnable.go
+++ src/utils/corsEnable.go
@@ -5,9 +5,19 @@ import (
 )
 
 func EnableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	allowedOrigins := map[string]bool{
+		"http://localhost:5173":           true,
+		"https://nodes-engine-frontend.vercel.app/":  true,
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
